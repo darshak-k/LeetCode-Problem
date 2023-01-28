77bad010bab16212a1be348e804c07a39bed0914
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2023-01-27 23:18:16 -0500

    Create 206_ReverseLinkedList.java

---
+++ 206_ReverseLinkedList.java
@@ -0,0 +1,33 @@
+/**
+206. Reverse Linked List
+Easy
+=========================================================================================
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+
+**/
+
+public ListNode reverseList(ListNode head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+
+  ListNode leftList = reverseList(head.next);
+  head.next.next = head;
+
+  head.next = null;
+  return leftList;
+}
